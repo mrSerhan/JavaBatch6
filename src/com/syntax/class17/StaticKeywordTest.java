555b4e0e92abf5e37b958316d90980e6092c55df
mrSerhan <mrserhan@hotmail.com>
2020-03-25 18:59:33 -0500

    Static and non static members

---
+++ src/com/syntax/class17/StaticKeywordTest.java
@@ -0,0 +1,14 @@
+package com.syntax.class17;
+
+public class StaticKeywordTest {
+
+	public static void main(String[] args) {
+		//access static members of another class
+		
+		StaticKeyword.brand = "Android";
+		StaticKeyword.touchScreen = true;
+		StaticKeyword.displayGeneralInfo();
+
+	}
+
+}
